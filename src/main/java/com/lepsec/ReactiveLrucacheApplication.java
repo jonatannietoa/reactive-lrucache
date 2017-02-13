6fe0435b6e254521daad53626d7f09e8dd94781f
jonatannietoa <jonatannietoa@gmail.com>
2017-02-13 19:21:28 +0100

    first commit

---
+++ src/main/java/com/lepsec/ReactiveLrucacheApplication.java
@@ -0,0 +1,12 @@
+package com.lepsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveLrucacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveLrucacheApplication.class, args);
+	}
+}
